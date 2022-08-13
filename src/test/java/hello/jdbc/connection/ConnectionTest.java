2270c76c8ee448684820581130bf028338a010b6
noah <m3252@naver.com>
2022-08-13 23:57:59 +0900

    test : HikariCP 커넥션 풀 사용 및 동작 확인

--- src/test/java/hello/jdbc/connection/ConnectionTest.java
+++ src/test/java/hello/jdbc/connection/ConnectionTest.java
@@ -1,5 +1,6 @@
 package hello.jdbc.connection;
 
+import com.zaxxer.hikari.HikariDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -27,9 +28,34 @@ public class ConnectionTest {
         useDataSource(dataSource);
     }
 
+    @Test
+    void dataSourceConnectionPool() throws Exception {
+        //커넥션 풀링
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+        dataSource.setMaximumPoolSize(10);
+        dataSource.setPoolName("MyPool");
+
+        useDataSource(dataSource);
+        Thread.sleep(1000L);
+    }
+
     private void useDataSource(DataSource dataSource) throws Exception{
         Connection con1 = dataSource.getConnection();
         Connection con2 = dataSource.getConnection();
+//        Connection con3 = dataSource.getConnection();
+//        Connection con4 = dataSource.getConnection();
+//        Connection con5 = dataSource.getConnection();
+//        Connection con6 = dataSource.getConnection();
+//        Connection con7 = dataSource.getConnection();
+//        Connection con8 = dataSource.getConnection();
+//        Connection con9 = dataSource.getConnection();
+//        Connection con10 = dataSource.getConnection();
+//        Connection con11 = dataSource.getConnection();
+        // MyPool - Fill pool skipped, pool is at sufficient level.
+        // java.sql.SQLTransientConnectionException: MyPool - Connection is not available, request timed out after 30002ms.
         log.info("connection={},class={}", con1, con1.getClass());
         log.info("connection={},class={}", con2, con2.getClass());
     }
